6741a5dc534c98e578b8432437e0880785012979
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-17 12:00:44 -0300

    test: teste

--- prisma/migrations/20240917144518_init/migration.sql
+++ prisma/migrations/20240917144518_init/migration.sql
@@ -3,109 +3,91 @@ CREATE TABLE "Users" (
     "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("userId")
 );
 
 -- CreateTable
 CREATE TABLE "Products" (
-    "productId" SERIAL NOT NULL,
+    "productId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "rating" DOUBLE PRECISION,
     "stockQuantity" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Products_pkey" PRIMARY KEY ("productId")
 );
 
 -- CreateTable
 CREATE TABLE "Sales" (
-    "saleId" SERIAL NOT NULL,
-    "productId" INTEGER NOT NULL,
+    "saleId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL,
     "quantity" INTEGER NOT NULL,
     "unitPrice" DOUBLE PRECISION NOT NULL,
     "totalAmount" DOUBLE PRECISION NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Sales_pkey" PRIMARY KEY ("saleId")
 );
 
 -- CreateTable
 CREATE TABLE "Purchases" (
-    "purchaseId" SERIAL NOT NULL,
-    "productId" INTEGER NOT NULL,
+    "purchaseId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL,
     "quantity" INTEGER NOT NULL,
     "unitCost" DOUBLE PRECISION NOT NULL,
     "totalCost" DOUBLE PRECISION NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Purchases_pkey" PRIMARY KEY ("purchaseId")
 );
 
 -- CreateTable
 CREATE TABLE "Expenses" (
-    "expenseId" SERIAL NOT NULL,
+    "expenseId" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Expenses_pkey" PRIMARY KEY ("expenseId")
 );
 
 -- CreateTable
 CREATE TABLE "SalesSummary" (
-    "salesSummaryId" SERIAL NOT NULL,
+    "salesSummaryId" TEXT NOT NULL,
     "totalValue" DOUBLE PRECISION NOT NULL,
     "changePercentage" DOUBLE PRECISION,
     "date" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "SalesSummary_pkey" PRIMARY KEY ("salesSummaryId")
 );
 
 -- CreateTable
 CREATE TABLE "PurchaseSummary" (
-    "purchaseSummaryId" SERIAL NOT NULL,
+    "purchaseSummaryId" TEXT NOT NULL,
     "totalPurchased" DOUBLE PRECISION NOT NULL,
     "changePercentage" DOUBLE PRECISION,
     "date" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "PurchaseSummary_pkey" PRIMARY KEY ("purchaseSummaryId")
 );
 
 -- CreateTable
 CREATE TABLE "ExpenseSummary" (
-    "expenseSummaryId" SERIAL NOT NULL,
+    "expenseSummaryId" TEXT NOT NULL,
     "totalExpenses" DOUBLE PRECISION NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "ExpenseSummary_pkey" PRIMARY KEY ("expenseSummaryId")
 );
 
 -- CreateTable
 CREATE TABLE "ExpenseByCategory" (
-    "expenseByCategoryId" SERIAL NOT NULL,
-    "expenseSummaryId" INTEGER NOT NULL,
+    "expenseByCategoryId" TEXT NOT NULL,
+    "expenseSummaryId" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "amount" BIGINT NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "ExpenseByCategory_pkey" PRIMARY KEY ("expenseByCategoryId")
 );
